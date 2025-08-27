573567dcbfc46591334c367d8fa92e396a9f54bd
Syvnox <84588449+nouixz@users.noreply.github.com>
2025-08-27 16:25:40 +0200

    Update and rename Read to Server.js

---
+++ Server.js
@@ -0,0 +1,13 @@
+// server.js
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello from Node inside a container! 🚀</h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
